e066e8239d7c04ddb98df173d6a90a38784aafcd
Alexander Nittka <alex@nittka.de>
2022-06-15 13:32:53 +0200

    use alternative AI from 0.7.3 release

---
+++ res/ai/OldAI/TaskBoss.lua
@@ -0,0 +1,163 @@
+-- File: TaskBoss
+-- >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+_G["TaskBoss"] = class(AITask, function(c)
+	AITask.init(c)	-- must init base!
+	c.Id = _G["TASK_BOSS"]
+	c.TargetRoom = TVT.ROOM_BOSS_PLAYER_ME
+	c.BudgetWeight = 0
+	c.BasePriority = 1
+	c.NeededInvestmentBudget = 0
+	c.InvestmentPriority = 0
+
+	c.GuessCreditAvailable = 100000
+	c.TryToGetCredit = 0
+	c.TryToRepayCredit = 0
+	c.LastMoodLevel = 5
+end)
+
+
+function TaskBoss:typename()
+	return "TaskBoss"
+end
+
+
+function TaskBoss:Activate()
+	-- Was getan werden soll:
+	self.CheckCreditJob = JobCheckCredit()
+	self.CheckCreditJob.Task = self
+	--self.LogLevel = LOG_TRACE
+end
+
+
+function TaskBoss:GetNextJobInTargetRoom()
+	if (self.CheckCreditJob.Status ~= JOB_STATUS_DONE) then
+		return self.CheckCreditJob
+	end
+
+	self:SetDone()
+end
+
+
+function TaskBoss:BeforeBudgetSetup()
+	self:CalculateFixedCosts()
+	self.InvestmentPriority = 0
+
+	local money = TVT.GetMoney()
+	local credit = MY.GetCredit(-1)
+	if (money - credit) > 350000 then
+		if credit > 100000 then
+			self.NeededInvestmentBudget = credit / 5
+			self.InvestmentPriority = 1
+		elseif credit > 0 then
+			self.NeededInvestmentBudget = credit
+			self.InvestmentPriority = 1
+		else
+			self.NeededInvestmentBudget = 0
+			self.CurrentInvestmentPriority = 0
+		end
+	elseif (money - credit) > 100000 then
+		self.NeededInvestmentBudget = credit / 10
+		self.InvestmentPriority = 1
+	elseif (credit > 0) then
+		self.NeededInvestmentBudget = 10000
+	else
+		self.NeededInvestmentBudget = 0
+	end
+end
+
+
+function TaskBoss:OnMoneyChanged(value, reason, reference)
+	reason = tonumber(reason)
+	if (reason == TVT.Constants.PlayerFinanceEntryType.CREDIT_TAKE) then
+		self:CalculateFixedCosts()
+	elseif (reason == TVT.Constants.PlayerFinanceEntryType.CREDIT_REPAY) then
+		self:CalculateFixedCosts()
+	end
+end
+
+
+-- update value of fixed costs (eg. credit interest)
+function TaskBoss:CalculateFixedCosts()
+	self.FixedCosts = MY.GetCreditInterest()
+end
+-- <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+
+
+
+-- >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+_G["JobCheckCredit"] = class(AIJob, function(c)
+	AIJob.init(c)	-- must init base!
+	c.Task = nil
+end)
+
+
+function JobCheckCredit:typename()
+	return "JobCheckCredit"
+end
+
+
+function JobCheckCredit:Prepare(pParams)
+	local money = TVT.GetMoney()
+	local creditAvailable = TVT.bo_getCreditAvailable()
+	if money < 0 then
+		-- ATTENTION: money might change until "tick()", we could handle
+		-- it but this behaviour seems more "natural" (to not see the
+		-- money change in time)
+		-- negative balance typically when the day begins (fixed costs, failed contracts);
+		-- try to achieve positive balance with minimal budget for buying news items
+		self.Task.TryToGetCredit = math.min( math.abs(money) + 100000, creditAvailable)
+	end
+	if self.Task.NeededInvestmentBudget > 0 then
+		self.Task.TryToRepayCredit = math.max(0, math.min(money, self.Task.NeededInvestmentBudget))
+	end
+
+
+	self.Task.GuessCreditAvailable = creditAvailable
+	self.Task.LastMoodLevel = TVT.bo_getBossMoodlevel()
+end
+
+
+function JobCheckCredit:Tick()
+	if self.Task.LastMoodLevel < 3 then
+		self:LogDebug("TODO: Boss in bad mood: " .. self.Task.LastMoodLevel)
+	end
+
+	-- REPAY credit
+	if self.Task.TryToRepayCredit > 0 then
+		local repay = self.Task.TryToRepayCredit
+		if repay > MY.GetCredit(-1) then
+			repay = MY.GetCredit(-1)
+		end
+
+		if TVT.bo_doRepayCredit(repay) == TVT.RESULT_OK then
+			self.Task.TryToRepayCredit = self.Task.TryToRepayCredit - repay
+			-- adjust budget
+			self.Task.NeededInvestmentBudget = self.Task.NeededInvestmentBudget - repay
+			self:LogDebug("Repaid " .. repay .. " from credit to boss.")
+		else
+			self:LogError("FAILED to repay " .. repay .. " from credit to boss.")
+		end
+
+	-- TAKE credit
+	elseif self.Task.TryToGetCredit > 0 then
+		local credit = self.Task.TryToGetCredit
+		if credit > self.Task.GuessCreditAvailable then
+			credit = self.Task.GuessCreditAvailable
+		end
+
+		if credit > 0 then
+			if TVT.bo_doTakeCredit(credit) == TVT.RESULT_OK then
+				self:LogInfo("Took a credit of " .. credit .." from boss.")
+			else
+				self:LogError("FAILED to get credit of " .. credit .." from boss.")
+			end
+
+			self.Task.TryToGetCredit = math.max(0, self.Task.TryToGetCredit - credit)
+		end
+	end
+	self.Status = JOB_STATUS_DONE
+end
+-- <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+--TODO: Auf schlechte Stimmung reagieren
